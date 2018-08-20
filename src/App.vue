87a5dee817cc224bb509dfa0c9914f01f0fb41d4
admiralman12 <mancinicw@outlook.com>
2018-08-19 18:35:46 -0600

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,111 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon 
+        @click.stop="drawer = !drawer"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">{{title}}</router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer> 
+      <!-- <v-toolbar-items class="hidden-xs-only"> -->
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+      <v-btn icon to="/properties">
+        <v-icon>notifications</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>account_circle</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer 
+      temporary fixed
+      v-model="drawer"
+      class="drawer"
+      dark
+    >
+      <v-list >
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+          :to="item.link"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <main>
+      <router-view></router-view>
+    </main>
+  </v-app>
 </template>
 
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      clipped: true,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+        icon: 'home',
+        title: 'Home',
+        link: '/'
+        },
+        {
+        icon: 'business',
+        title: 'Properties',
+        link: '/properties'
+        },
+        {
+        icon: 'link',
+        title: 'Connections',
+        link: '/connections'
+        },
+        {
+        icon: 'star',
+        title: 'Ratings',
+        link: '/properties'
+        },        
+        {
+        icon: 'list',
+        title: 'Activity',
+        link: '/activity'
+        },
+        {
+        icon: 'bar_chart',
+        title: 'Reports',
+        link: '/reports'
+        },
+        {
+        icon: 'notifications',
+        title: 'Notifications',
+        link: '/notifications'
+        },
+        {
+        icon: 'account_circle',
+        title: 'Profile'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Connect'
+    }
+  }
+}
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.drawer {
+  background-color: #424242 !important;
 }
 </style>
